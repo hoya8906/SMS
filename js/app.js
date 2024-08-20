ce77a5894e8b8bf16f2f0d2e82e1e2abf3cbf24f
hoya8906 <darkkuho@nate.com>
2024-08-21 08:55:58 +0900

    코드 가독성이 안좋아 수정은 좀 그렇고 주석이라도..

--- js/app.js
+++ js/app.js
@@ -27,7 +27,6 @@ let opt = 1;
 if (localStorage.getItem("students")) students = JSON.parse(localStorage.getItem("students"));
 else students = [];
 
-// 모달 창 출력  관련
 // 모달 열기 함수
 function showModal(message) {
     const modalBodyText = document.getElementById("modal-body-text");
@@ -38,7 +37,7 @@ function showModal(message) {
     myModal.show();
 }
 
-// 목록 정렬
+// 목록 정렬 : 옵션 값에 따라 여러 방법으로 정렬한 배열을 반환
 function sortList(array, opt) {
 
     switch (opt) {
@@ -53,7 +52,7 @@ function sortList(array, opt) {
     }
 };
 
-// 헤더 출력
+// 헤더 출력 : 화면 내용이 없을 때 화면을 깔끔하게 하기 위해 헤더 출력함수 별도 정의
 function printHeader() {
     clearList();
     const showList = document.querySelector(".table");
@@ -78,6 +77,7 @@ function printList(inputValue, option, searchType) {
     let tempArray = [];
 
     if (option > 0) {
+        // 검색창에 입력값이 있을 때
         students.forEach((student) => {
             // 전체 일치 필터링
             // if ((option === 1 ? String(student.sno) : student.name) === inputValue) {
@@ -91,9 +91,11 @@ function printList(inputValue, option, searchType) {
         }
         );
     } else {
+        // 검색창에 입력값이 없을 때(전체 검색)
         students.forEach(element => tempArray.push(element));
     };
 
+    // 출력을 위해 기준에 따른 정렬
     tempArray = sortList(tempArray, searchType);
 
 
@@ -103,6 +105,7 @@ function printList(inputValue, option, searchType) {
         const showList = document.querySelector(".table");
         const tBody = document.createElement("tbody");
 
+        // 행 만들기
         tempArray.forEach(element => {
             const newList = document.createElement("tr");
 
@@ -117,6 +120,7 @@ function printList(inputValue, option, searchType) {
                 element.rank,
             ];
 
+            // 열 만들기
             values.forEach(value => {
                 const cell = document.createElement("td");
                 cell.textContent = value;
@@ -136,14 +140,13 @@ function printList(inputValue, option, searchType) {
     }
 };
 
-// 목록 삭제
+// 목록 삭제 : 다음 출력을 위해, 화면을 깨끗이!
 const clearList = function () {
-    const tempList = document.querySelectorAll("td");
-    tempList.forEach(element => element.remove());
+    document.querySelectorAll("td").forEach(td => td.remove());
     document.querySelectorAll("p").forEach(p => p.remove());
 }
 
-// 입력창 비우기
+// 입력창 비우기 : 등록, 삭제 후 입력창을 깨끗이!
 const clearInput = function () {
     studentInput.forEach(element => element.value = "")
 };
@@ -154,6 +157,7 @@ const saveStorage = function () {
 }
 
 // 학생 등록
+// 입력값 검증을 통과했으며, 이미 등록한 학번이 아닐 때:
 const saveStudent = function (student, no) {
     if (no) {
         students.push(student);
@@ -190,12 +194,13 @@ const addStudent = function () {
     if (pass === 0) showModal("입력한 내용을 다시 확인해주세요.");
 
     if (pass === 1) {
+        // 동일한 번호가 이미 있으면:
         students.forEach((element) => {
             if (parseInt(element.sno) === parseInt(sno)) {
                 pass = 0
             }
         });
-        if (pass === 0) showModal("이미 등록된 학번입니다.");
+        if (pass === 0) showModal("이미 등록된 번호입니다.");
     }
 
     if (pass === 1) {
@@ -235,10 +240,11 @@ const delStudent = function (no) {
         };
     };
 
+    // 삭제하고자 하는 번호를 students에서 찾을 수 없으면:
     if (error === 1) showModal("삭제하고자 하는 학생 번호를 바르게 입력해주세요.");
 };
 
-// 순위 산출 재설정
+// 랭크 매기는 함수
 const setRank = function () {
     const rankArray = sortList(students);
     for (let i = 0; i < rankArray.length; i++) {
@@ -251,7 +257,6 @@ const setRank = function () {
 setRank();
 
 // 이벤트 등록
-
 // 학생 등록 버튼
 addStudentBtn.addEventListener("click", () => {
     addStudent();
